ee7ae42992c67cb10acb7f163fe765ae7aa3dddd
Otavio Gaiao <otavio.gaiao@al.go.leg.br>
2018-04-27 15:04:17 -0300

    adding picker

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,1 +1,77 @@
 import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Button, Card } from "react-native-elements";
+
+class QuestionBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.answer = this.answer.bind(this);
+  }
+
+  state = {
+    allowedToDrink: null,
+    answered: false
+  }
+
+  answer(allowedToDrink) {
+    this.setState({answered: true, allowedToDrink});
+  }
+
+  render() {
+    const { allowedToDrink, answered } = this.state
+    if(!answered) {
+      return (
+        <Card wrapperStyle={styles.container}>
+          <Text h4 style={styles.questionText}>Are you at least 18 years old? </Text>
+          <View style={styles.buttonsContainer}>
+            <Button
+              title="Yes"
+              icon={{name:'thumb-up'}}
+              buttonStyle={[styles.button, {backgroundColor: '#2ecc71'}]} onPress={() => this.answer(true)}
+             />
+            <Button
+              title="No"
+              icon={{name:'thumb-down'}}
+              buttonStyle={[styles.button, {backgroundColor: '#d35400'}]} onPress={() => this.answer(false)}
+            />
+          </View>
+        </Card>
+      )
+    }
+
+    return (
+        <Card wrapperStyle={styles.container}>
+          <Text style={[styles.answerText, {color: allowedToDrink ? '#27ae60' : '#c0392b'}]}>
+            {allowedToDrink ? 'You are allowed to drink' : 'You are not allowed to drink'}
+          </Text>
+        </Card>
+    )
+  }
+}
+
+export default QuestionBox;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    // minHeight: 150,
+    minWidth: '90%'
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  button: {
+    width: 90
+  },
+  questionText: {
+    textAlign: 'center',
+    marginBottom: 30
+  },
+  answerText: {
+    textAlign: 'center',
+    fontSize: 20
+  }
+})
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,12 +1,55 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Picker } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 
+import QuestionBox from "../components/QuestionBox";
+
+const values = ['Apple', 'Orange', 'Banana', 'Grape'];
+
 class MainScreen extends Component {
+  constructor(props) {
+    super(props)
+    this.updateValue = this.updateValue.bind(this)
+  }
+
+  state = {
+    selectedValue: null,
+    showingPicker: false
+  }
+
+  updateValue(value, index) {
+    this.setState({ selectedValue: value, showingPicker: false });
+  }
+
   render() {
+    // const fruits = ['Apple', 'Orange', 'Banana', 'Grape'];
+    console.log(this.state)
     return (
       <View style={styles.container}>
-        <Text>MainScreen</Text>
+        <QuestionBox />
+        <Picker
+           style={styles.picker}
+           itemStyle={styles.pickerItem}
+           testId='picker'
+           selectedValue={this.state.selectedValue}
+           onValueChange={this.updateValue}
+        >
+             <Picker.Item
+                 key={0}
+                 label="Choose a fruit"
+                 value={null}
+             />
+
+             {values.map(value => {
+                 return (
+                     <Picker.Item
+                         key={value}
+                         label={value}
+                         value={value}
+                     />
+                 );
+             })}
+           </Picker>
       </View>
     );
   }
@@ -18,6 +61,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    // alignItems: "center"
+  },
+  picker: {
+    backgroundColor: 'white'
+  },
+  pickerItem: {
+    height: 150
   }
 });
